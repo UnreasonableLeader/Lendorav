6db84588c644b80ada1ed277cda447a40236df54
Sofia Apri <sofiaapr@ut.ee>
2024-12-02 10:28:09 +0200

    valideeritud ja kommenteeritud

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// Lisab nupule klikisündmuse
+// Lisab nupule klikisündmuse https://www.w3schools.com/js/js_htmldom_eventlistener.asp
 document.getElementById('annetuse_nupp').addEventListener('click', function() {
-    alert('Aitäh, et toetad lendoravaid!')
-    // Suunab kasutaja annetuste lehele
+    alert('Aitäh, et toetad lendoravaid!');
+    // Suunab kasutaja annetuste lehele https://www.w3schools.com/js/js_window_location.asp
     window.location.href = 'https://elfond.ee/anneta';
   });
+
+
+
+
 
 // Leia kõik sektsioonid
 const boxes = document.querySelectorAll('.box');
 
+
+// Sofia JavaScript
 // Funktsioon, mis kontrollib, kas sektsioon on vaateväljas
 function showBoxes() {
     const triggerBottom = window.innerHeight / 1.2; // Positsioon, kus lõik muutub nähtavaks
@@ -28,4 +34,3 @@ window.addEventListener('scroll', showBoxes);
 
 // Esialgne kontroll (kui lõigud on juba nähtaval)
 showBoxes();
-
