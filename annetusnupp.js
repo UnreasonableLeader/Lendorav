3fee8b2b1645c52cccaa90dc7fbf95d47a123e97
Sofia Apri <sofiaapr@ut.ee>
2024-12-02 17:19:58 +0200

    ühine css

---
+++ annetusnupp.js
@@ -0,0 +1,6 @@
+// Lisab nupule klikisündmuse https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+document.getElementById('annetuse_nupp').addEventListener('click', function() {
+    alert('Aitäh, et toetad lendoravaid!');
+    // Suunab kasutaja annetuste lehele https://www.w3schools.com/js/js_window_location.asp
+    window.location.href = 'https://elfond.ee/anneta';
+  });
