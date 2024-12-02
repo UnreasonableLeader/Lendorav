e368d9da014af00748aa0e57d0467d3fe5f52a97
unknown <emma.laikmaa@gmail.com>
2024-12-02 18:11:40 +0200

    lisa

--- karusell.js
+++ karusell.js
@@ -7,13 +7,13 @@ const nextButton = document.querySelector('.next-button');
 
 let currentIndex = 0;
 
-// Function to show the current slide
+// Funktsioon mis näitab slaidi mis sel hetkel ees
 function showSlide(index) {
     currentIndex = (index + slides.length) % slides.length;
     carousel.style.transform = `translateX(-${index * 100}%)`;
 }
 
-// Event listeners for prev and next buttons
+// vaatab kas nuppe on vajutatud
 prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
     showSlide(currentIndex);
@@ -24,7 +24,7 @@ nextButton.addEventListener('click', () => {
     showSlide(currentIndex);
 });
 
-// Initial slide
+// 
 showSlide(currentIndex);
 
 /* allikas https://skillupwards.com/blog/building-a-carousel-slider-with-javascript-and-css , muutsin natuke ChatGPT'ga */
